11e77cffed373a1c48f5daace5c8733c353121d6
Jeswin Kumar <jeswinpk@agilehead.com>
2026-02-16 09:29:10 +0530

    fix(frontend): contextual typing for delegate-typed function expressions

--- test/fixtures/continuewith-return-task-dotnet/packages/continuewith-return-task-dotnet/src/index.ts
+++ test/fixtures/continuewith-return-task-dotnet/packages/continuewith-return-task-dotnet/src/index.ts
@@ -2,10 +2,21 @@ import { Console } from "@tsonic/dotnet/System.js";
 import { Task } from "@tsonic/dotnet/System.Threading.Tasks.js";
 
 export function main(): void {
-  const t = Task.CompletedTask.ContinueWith<Task>((_t, _state) => {
-    Console.WriteLine("CONT");
+  const t1 = Task.CompletedTask.ContinueWith<Task>((_t, _state) => {
+    Console.WriteLine("CONT-1");
     return Task.CompletedTask;
   }, undefined);
-  t.Wait();
-}
+  t1.Wait();
 
+  const t2 = Task.CompletedTask.ContinueWith<Task>((_t: Task, _state: unknown) => {
+    Console.WriteLine("CONT-2");
+    return Task.CompletedTask;
+  }, undefined);
+  t2.Wait();
+
+  const t3 = Task.CompletedTask.ContinueWith<Task>(function (_t: Task, _state: unknown) {
+    Console.WriteLine("CONT-3");
+    return Task.CompletedTask;
+  }, undefined);
+  t3.Wait();
+}
